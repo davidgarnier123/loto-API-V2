db945ba5fa03f9b104673a117f618962dbb9c15c
DaGa123 <DaGa123>
2023-03-31 08:45:08 +0200

    fix update var

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ async function addResultsToDatabase(results) {
     const resultsCollection = myDatabase.collection('results');
     const result = await resultsCollection.insertMany(results);
     console.log(`Added ${result.insertedCount} results to database`);
+    results = await resultsCollection.find({}).toArray();
 }
 
 function deleteAllResults() {
